584ebf21426458cc1637d4af8132cd433914b53b
ha666 <ha666@ha666.com>
2020-09-04 17:07:28 +0800

    读取消息区分是否自动重连

--- read.go
+++ read.go
@@ -6,10 +6,11 @@ import (
 	"time"
 
 	"github.com/golang/protobuf/proto"
+	"github.com/gorilla/websocket"
 	"github.com/maurodelazeri/gorilla-reconnect"
 )
 
-func ReadMessage(c *recws.RecConn) (dst proto.Message, messageType []byte, err error) {
+func ReadMessageWithAutoConnect(c *recws.RecConn) (dst proto.Message, messageType []byte, err error) {
 	defer func() {
 		if exception := recover(); exception != nil {
 			err = errors.New(fmt.Sprintf("ReadMessage异常:%v", exception))
@@ -21,6 +22,25 @@ func ReadMessage(c *recws.RecConn) (dst proto.Message, messageType []byte, err e
 		err = errors.New("ReadMessage错误:连接已断开")
 		return
 	}
+	_, message, err := c.ReadMessage()
+	if err != nil {
+		time.Sleep(30 * time.Millisecond)
+		err = errors.New("ReadMessage错误:" + err.Error())
+		return
+	}
+	if message == nil || len(message) < 16 {
+		return nil, nil, errors.New("无效的消息")
+	}
+	return decode(message)
+}
+
+func ReadMessage(c *websocket.Conn) (dst proto.Message, messageType []byte, err error) {
+	defer func() {
+		if exception := recover(); exception != nil {
+			err = errors.New(fmt.Sprintf("ReadMessage异常:%v", exception))
+			return
+		}
+	}()
 	_, message, err := c.ReadMessage()
 	if err != nil {
 		time.Sleep(30 * time.Millisecond)
